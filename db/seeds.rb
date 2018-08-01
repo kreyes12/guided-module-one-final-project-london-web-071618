ae00dd7bdc5002050e3640f62f19c9f486f58af1
kreyes12 <karla.y.reyes12@gmail.com>
2018-08-01 16:28:10 +0100

    deleted more_data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,34 @@
 require 'json'
 require 'net/http'
 require "pry"
-
-def get_characters
-  # url = 'https://anapioficeandfire.com/api/characters/?page=2&pageSize=50'
-  # uri = URI(url)
-  # response = Net::HTTP.get(uri)
-  # json_array = JSON.parse(response)
-  #
-  # binding.pry
-  # 0
-  i = 1
-  characters = []
+require_relative './characters_data.rb'
 
 
-  while i <= 43
-    i += 1
-    url = "https://anapioficeandfire.com/api/characters/?page=#{i}&pageSize"
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    characters_array = JSON.parse(response)
-    characters += characters_array
-  end
-
-  characters
-end
-
-character_array = []
-character_array += get_characters
 
 
-character_array.each do |character|
-  character.each do |next_level|
-  Character.create(name: character["name"] url: character["url"])
-end
-
-# binding.pry
-# 0
+# def get_characters
+#   # url = 'https://anapioficeandfire.com/api/characters/?page=2&pageSize=50'
+#   # uri = URI(url)
+#   # response = Net::HTTP.get(uri)
+#   # json_array = JSON.parse(response)
+#   #
+#   # binding.pry
+#   # 0
+#   i = 1
+#   characters = []
+#
+#
+#   while i <= 43
+#     url = "https://anapioficeandfire.com/api/characters/?page=#{i}&pageSize"
+#     uri = URI(url)
+#     response = Net::HTTP.get(uri)
+#     characters_array = JSON.parse(response)
+#     characters += characters_array
+#     i += 1
+#   end
+#
+#   p characters
+# end
+#
+#
+# get_characters
