9b23ce069ea6bd79eab7333e6725654b6d8e1585
kreyes12 <karla.y.reyes12@gmail.com>
2018-08-01 16:57:55 +0100

    added books data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,46 @@
 require 'json'
 require 'net/http'
 require "pry"
-require_relative './characters_data.rb'
+
+def get_characters
+  # url = 'https://anapioficeandfire.com/api/characters/?page=2&pageSize=50'
+  # uri = URI(url)
+  # response = Net::HTTP.get(uri)
+  # json_array = JSON.parse(response)
+  #
+  # binding.pry
+  # 0
+  i = 1
+  characters = []
+
+
+  while i <= 43
+    url = "https://anapioficeandfire.com/api/characters/?page=#{i}&pageSize"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    characters_array = JSON.parse(response)
+    characters += characters_array
+    i += 1
+  end
+
+  characters
+end
+
+character_array = []
+character_array += get_characters
+
+  def get_books
+    books_url = "https://anapioficeandfire.com/api/books/?page=1&pageSize=50"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    books_array = JSON.parse(response)
+    books += books_array
+end
+
+book_array = []
+book_array += get_books
 
 
 
-
-# def get_characters
-#   # url = 'https://anapioficeandfire.com/api/characters/?page=2&pageSize=50'
-#   # uri = URI(url)
-#   # response = Net::HTTP.get(uri)
-#   # json_array = JSON.parse(response)
-#   #
-#   # binding.pry
-#   # 0
-#   i = 1
-#   characters = []
-#
-#
-#   while i <= 43
-#     url = "https://anapioficeandfire.com/api/characters/?page=#{i}&pageSize"
-#     uri = URI(url)
-#     response = Net::HTTP.get(uri)
-#     characters_array = JSON.parse(response)
-#     characters += characters_array
-#     i += 1
-#   end
-#
-#   p characters
-# end
-#
-#
-# get_characters
+get_characters
+get_books
