27b095944539031ae74620b861d45f689c71927e
Adham <admuaq@gmail.com>
2018-08-01 14:17:46 +0100

    linking seeds file to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,30 @@
 require 'json'
 require 'net/http'
+require "pry"
 
 def get_characters
-url = 'https://anapioficeandfire.com/api/characters/?page=2&pageSize=50'
-uri = URI(url)
-response = Net::HTTP.get(uri)
-json_array = JSON.parse(response)
+  # url = 'https://anapioficeandfire.com/api/characters/?page=2&pageSize=50'
+  # uri = URI(url)
+  # response = Net::HTTP.get(uri)
+  # json_array = JSON.parse(response)
+  #
+  # binding.pry
+  # 0
+  i = 1
+  characters = []
 
 
+  while i <= 43
+    url = "https://anapioficeandfire.com/api/characters/?page=#{i}&pageSize"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    characters_array = JSON.parse(response)
+    characters += characters_array
+    i += 1
+  end
 
-
-
+  p characters
 end
+
+
+get_characters
